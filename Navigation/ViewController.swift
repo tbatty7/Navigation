a9a338276887bf6669befa0de3b7f51d3b02e901
Tim Batty <tbatty7@gmail.com>
2022-02-07 09:33:04 -0500

    Implemented buttons navigating to a code-based ViewController and sending text using a push and a modal

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private(set) var codePushButton: UIButton!
+    @IBOutlet private(set) var codeModalButton: UIButton!
+    @IBOutlet private(set) var seguePushButton: UIButton!
+    @IBOutlet private(set) var segueModalButton: UIButton!
+
+    @IBAction private func pushNextViewController() {
+        let nextVC = CodeNextViewController(labelText: "Pushed from code")
+        self.navigationController?
+            .pushViewController(nextVC, animated: true)
     }
-
-
+    
+    @IBAction private func presentModalNextViewController() {
+        let nextVC = CodeNextViewController(labelText: "Modal from code")
+        self.present(nextVC, animated: true)
+    }
 }
 
