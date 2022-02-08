9896ee5df427169f5c0bac2d0226d0b112510f79
Tim Batty <tbatty7@gmail.com>
2022-02-08 10:08:37 -0500

    Added TestableViewController, to be used as a spy to test things like modals, cannot be used with storyboard-based ViewControllers

--- NavigationTests/ViewControllerTests.swift
+++ NavigationTests/ViewControllerTests.swift
@@ -44,3 +44,17 @@ final class ViewControllerTests: XCTestCase {
     }
 }
 
+private class TestableViewController: ViewController {
+    var presentCallCount = 0
+    var presentArgsViewController: [UIViewController] = []
+    var presentArgsAnimated: [Bool] = []
+    var presentArgsClosure: [(()->Void)?] = []
+    
+    override func present(_ viewControllerToPresent: UIViewController,
+                          animated flag: Bool,
+                          completion: (()->Void)? = nil) {
+        presentCallCount += 1
+        presentArgsViewController.append(viewControllerToPresent)
+        presentArgsAnimated.append(flag)
+    }
+}
